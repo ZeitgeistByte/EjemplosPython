a0b7b7575a332ae2ad23cefd0800a823cfba1517
David Juárez <34253527+ZeitgeistByte@users.noreply.github.com>
2020-06-10 12:46:59 -0600

    CAmbios en flash

--- flash.py
+++ flash.py
@@ -34,13 +34,57 @@ def ejercicio_3():
     lista = []
     i = len(lista)
     n = 2
-    while i < 6:
+    while i < 702:
         if es_primo(n): #Si el numero es primo lo agrega a la sumatoria
             lista.append(n)
             i = len(lista)
         n = n + 1
     return  lista[700]#Envia el resultado
 
+###########################################################################################
+def es_abundante(N):
+    lista_divisores = []
+    for i in range (1,N):
+        if N % i == 0:
+            lista_divisores.append(i)
+    if(sum(lista_divisores) > N): #Si la suma de divisores > N
+        return True
+    else:
+        return False
+
+
+def ejercicio_4():
+    lista_abundantes = []
+    i = len(lista_abundantes)
+    n = 2    
+    while i < 778:
+        if es_abundante(n):
+            lista_abundantes.append(n) # Si es abundante lo agrega a la lista
+            i = len(lista_abundantes)
+        n = n + 1            
+    return lista_abundantes[776] #El 777 es el indice 776
+
+###########################################################################################
+def es_perfecto(N):
+    lista_divisores = []
+    for i in range (1,N):
+        if N % i == 0:
+            lista_divisores.append(i)
+    if(sum(lista_divisores) == N): #Perfecto si sumatoria de divisores = N
+        return True
+    else:
+        return False
+
+def ejercicio_5():
+    lista_perfectos = []
+    for i in range (1,7944):
+        if es_perfecto(i): #si es perfecto lo agrega a la lista
+            lista_perfectos.append(i)   
+    return sum(lista_perfectos) #Envia la sumatoria
+
+
 print('La diferencia para los primeros 144 numeros naturales es ',ejercicio_1())
 print('La sumatoria de los numeros primos debajo de 7106 son  ',ejercicio_2(7106))
 print('El 701-ésimo numero primo es  ',ejercicio_3())
+print('El 777-ésimo numero abundante es  ',ejercicio_4())
+print('La sumatoria de todos los números perfectos debajo de 7944 es ',ejercicio_5())
